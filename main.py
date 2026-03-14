7fe30ff1f841561f6f2ab0a82fd50f04b98a0e8d
Rosandi <54345956+BigBoz97@users.noreply.github.com>
2026-03-14 15:51:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-print("Script-hantu running...")
+import json
+import asyncio
+from telethon import TelegramClient
 
-def main():
-    print("Hello from Script-hantu")
+def banner():
+    print("=================================")
+    print(" ClickBee LTC Bot Skeleton ")
+    print("=================================")
+
+async def main():
+
+    banner()
+
+    config = json.load(open("config.json"))
+
+    api_id = config["api_id"]
+    api_hash = config["api_hash"]
+    bot_username = config["bot_username"]
+
+    client = TelegramClient("session/session", api_id, api_hash)
+
+    await client.start()
+
+    print("Login success")
+
+    await client.send_message(bot_username, "/start")
+
+    print(f"Connected to @{bot_username}")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
